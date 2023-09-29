1a02ea706048f7ba485696e2a7cbd0299b01345a
rafbar34 <rafbar34@gmail.com>
2023-09-29 08:11:05 +0200

    added dot env

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 import express from 'express';
+import morgan from 'morgan';
+
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('test');
 });
+app.post('/', (req, res) => {
+  console.log(req);
+  res.json({message: 'data recieved', data: req.body});
+});
 
-app.listen(1996, () => {
+app.listen(process.env.PORT, () => {
   console.log('server Runneing...');
 });
