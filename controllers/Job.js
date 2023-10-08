2f144c16c4c6ea562dd779c9a635ba7125465d4c
rafbar34 <rafbar34@gmail.com>
2023-10-08 22:02:19 +0200

    added try/catch to job model

--- controllers/Job.js
+++ controllers/Job.js
@@ -7,54 +7,71 @@ let jobs = [
 ];
 
 export const getAllJobs = async (req, res) => {
-  res.status(200).json({jobs});
+  try {
+    res.status(200).json({jobs});
+  } catch (err) {
+    res.status(500).json({msg: 'server error', reason: err});
+  }
 };
 export const createNewJob = async (req, res) => {
-  const newJob = await JobModel.create(req.body);
-  res.status(201).json({newJob});
-}
+  try {
+    const newJob = await JobModel.create(req.body);
+    res.status(201).json({newJob});
+  } catch (err) {
+    res.status(500).json({msg: 'server error', reason: err});
+  }
+};
 
 export const getSingleJob = async (req, res) => {
   const {id} = req.params;
-
-  const singleJob = jobs.find((job) => job.id === id);
-  if (!singleJob) {
-    throw new Error('job with this id isnt exist');
-  } else {
-    res.status(200).json({job: singleJob});
+  try {
+    const singleJob = jobs.find((job) => job.id === id);
+    if (!singleJob) {
+      throw new Error('job with this id isnt exist');
+    } else {
+      res.status(200).json({job: singleJob});
+    }
+  } catch (err) {
+    res.status(500).json({msg: 'server error', reason: err});
   }
 };
 
 export const updateJob = (req, res) => {
   const {id} = req.params;
   const {company, position} = req.body;
-
-  if (!company || !position) {
-    return res.status(400).json({message: 'Error, some values are empty'});
-  }
-  const singleJob = jobs.find((job) => job.id === id);
-  if (!singleJob) {
-    return res.status(404).json({message: 'Error, Job dosent exist'});
-  } else {
-    const otherJobs = jobs.filter((job) => job.id !== id);
-    const editedJob = {
-      id: id,
-      company,
-      position,
-    };
-    const modificatiedArray = [...otherJobs, editedJob];
-    res.status(200).json({job: modificatiedArray});
+  try {
+    if (!company || !position) {
+      return res.status(400).json({message: 'Error, some values are empty'});
+    }
+    const singleJob = jobs.find((job) => job.id === id);
+    if (!singleJob) {
+      return res.status(404).json({message: 'Error, Job dosent exist'});
+    } else {
+      const otherJobs = jobs.filter((job) => job.id !== id);
+      const editedJob = {
+        id: id,
+        company,
+        position,
+      };
+      const modificatiedArray = [...otherJobs, editedJob];
+      res.status(200).json({job: modificatiedArray});
+    }
+  } catch {
+    res.status(500).json({msg: 'server error', reason: err});
   }
 };
 
 export const deleteJob = (req, res) => {
   const {id} = req.params;
-
-  const singleJob = jobs.find((job) => job.id === id);
-  if (!singleJob) {
-    return res.status(404).json({message: 'Error, Job dosent exist'});
-  } else {
-    jobs.filter((job) => job.id !== id);
-    res.status(200).json({message: 'job has been deleted'});
+  try {
+    const singleJob = jobs.find((job) => job.id === id);
+    if (!singleJob) {
+      return res.status(404).json({message: 'Error, Job dosent exist'});
+    } else {
+      jobs.filter((job) => job.id !== id);
+      res.status(200).json({message: 'job has been deleted'});
+    }
+  } catch (err) {
+    res.status(500).json({msg: 'server error', reason: err});
   }
 };
