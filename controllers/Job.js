fa473190c41452ca1c5a727eb5a398c945f11fc9
rafbar34 <rafbar34@gmail.com>
2023-10-10 20:14:11 +0200

    added status code

--- controllers/Job.js
+++ controllers/Job.js
@@ -1,19 +1,19 @@
 import {nanoid} from 'nanoid';
 import JobModel from '../models/JobModel.js';
-
+import { StatusCodes } from 'http-status-codes';
 
 export const getAllJobs = async (req, res) => {
   try {
     const jobs = await JobModel.find({});
-    res.status(200).json({jobs});
+    res.status(StatusCodes.OK).json({jobs});
   } catch (err) {
-    res.status(500).json({msg: 'server error', reason: err});
+    res.status(StatusCodes.GATEWAY_TIMEOUT).json({msg: 'server error', reason: err});
   }
 };
 export const createNewJob = async (req, res) => {
   try {
     const newJob = await JobModel.create(req.body);
-    res.status(201).json({newJob});
+    res.status(StatusCodes.CREATED).json({newJob});
   } catch (err) {
     res.status(500).json({msg: 'server error', reason: err});
   }
@@ -26,7 +26,7 @@ export const getSingleJob = async (req, res) => {
     if (!singleJob) {
       throw new Error('job with this id isnt exist');
     } else {
-      res.status(200).json({job: singleJob});
+      res.status(StatusCodes.OK).json({job: singleJob});
     }
   } catch (err) {
     res.status(500).json({msg: 'server error', reason: err});
@@ -37,13 +37,13 @@ export const updateJob = async (req, res) => {
   const {id} = req.params;
   try {
     if (!company || !position) {
-      return res.status(400).json({message: 'Error, some values are empty'});
+      return res.status(StatusCodes.BAD_REQUEST).json({message: 'Error, some values are empty'});
     }
     const updatedJob = await JobModel.findByIdAndUpdate(id, req.body, {
       new: true,
     });
     if (!updatedJob) {
-      return res.status(404).json({message: 'Error, Job dosent exist'});
+      return res.status(StatusCodes.NOT_FOUND).json({message: 'Error, Job dosent exist'});
     } else {
       const otherJobs = jobs.filter((job) => job.id !== id);
       const editedJob = {
@@ -52,7 +52,7 @@ export const updateJob = async (req, res) => {
         position,
       };
       const modificatiedArray = [...otherJobs, editedJob];
-      res.status(200).json({job: modificatiedArray});
+      res.status(StatusCodes.OK).json({job: modificatiedArray});
     }
   } catch {
     res.status(500).json({msg: 'server error', reason: err});
@@ -64,9 +64,9 @@ export const deleteJob = async (req, res) => {
   const removedJob = await JobModel.findByIdAndDelete(id);
   try {
     if (!removedJob) {
-      return res.status(404).json({message: 'Error, Job dosent exist'});
+      return res.status(StatusCodes.NOT_FOUND).json({message: 'Error, Job dosent exist'});
     } else {
-      res.status(200).json({message: 'job has been deleted', job: removedJob});
+      res.status(StatusCodes.OK).json({message: 'job has been deleted', job: removedJob});
     }
   } catch (err) {
     res.status(500).json({msg: 'server error', reason: err});
