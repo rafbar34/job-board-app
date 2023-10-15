6640b2082c713dfc66ee47a1aae1ac5bef15e7c8
rafbar34 <rafbar34@gmail.com>
2023-10-15 15:26:52 +0200

    added consts

---
+++ routes/utils/constants.js
@@ -0,0 +1,19 @@
+export const JOB_STATUS = {
+    PENDING:'pending',
+    INTERVIEW:'interview',
+    DECLINED:'declined'
+}
+
+export const JOB_TYPE = {
+    FULL_TIME:'full-time',
+    PART_TIME:'part-time',
+    INTERSHIP:'intership'
+}
+
+export const SORT_BY ={
+    NEWEST_FIRST:'newest',
+    OLDEST_FIRST:'oldest',
+    A_Z:'a-z',
+    Z_A:'z-a'
+
+}
