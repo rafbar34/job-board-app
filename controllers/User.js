d341ee138d59bc66de2e6d8e447c68068b8e0f56
rafbar34 <rafbar34@gmail.com>
2023-10-19 14:12:47 +0200

    finished update user

--- controllers/User.js
+++ controllers/User.js
@@ -3,16 +3,18 @@ import UserModel from '../models/UserModel.js';
 
 export const getCurrentUser = async (req, res) => {
   const user = await UserModel.findOne({_id: req.user.userId});
-  const userWithoutPassword = user.toJSON()
+  const userWithoutPassword = user.toJSON();
   console.log(user, 'user');
   if (user) {
-    res.status(StatusCodes.OK).json({user:userWithoutPassword});
+    res.status(StatusCodes.OK).json({user: userWithoutPassword});
   }
 };
 export const getApplicationStats = async (req, res) => {
   res.status(StatusCodes.OK).json({msg: 'get current user'});
 };
 export const updateUser = async (req, res) => {
-  const updatedUser = 
+  const obj = {...req.body};
+  delete obj.password;
+  const updatedUser = await UserModel.findByIdAndUpdate(req.user.userId, obj);
   res.status(StatusCodes.OK).json({msg: 'update user'});
 };
